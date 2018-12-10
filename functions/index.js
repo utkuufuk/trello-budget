a5bfa8d340b8f59195ab5c261f80c4d625b49c32
utkuufuk <utkuufuk@gmail.com>
2018-12-11 01:58:00 +0300

    use strict mode index.js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
+'use strict';
+
 const functions = require('firebase-functions');
 
 exports.transaction = functions.https.onRequest((request, response) => {
-    if (!('body' in request) || !('action' in request.body) || !('type' in request.body.action))
-    {
+    if (!('body' in request) || !('action' in request.body) || !('type' in request.body.action)) {
         console.warn("Invalid Trello webhook request.");
         return response.status(400).end();
     }
