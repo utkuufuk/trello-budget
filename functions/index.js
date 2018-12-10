7186398bdfe54042355214919eb93fd74b1abc3b
utkuufuk <utkuufuk@gmail.com>
2018-12-11 01:53:29 +0300

    validate webhook request data attributes

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require('firebase-functions');
 
 exports.transaction = functions.https.onRequest((request, response) => {
-    console.log(request.body);
-    return response.status(200).send("Hello from Firebase!");
+    if (!('body' in request) || !('action' in request.body) || !('type' in request.body.action))
+    {
+        console.warn("Invalid Trello webhook request.");
+        return response.status(400).end();
+    }
+    console.log("Card Name:", request.body.action.data.card.name);
+    return response.status(200).end();
 });
