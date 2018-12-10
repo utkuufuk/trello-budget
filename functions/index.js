976fed475f5c41762e9e3a8464ea38494e3bf769
utkuufuk <utkuufuk@gmail.com>
2018-12-10 23:11:27 +0300

    create the first http cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.transaction = functions.https.onRequest((request, response) => {
+    console.log(request.body);
+    return response.status(200).send("Hello from Firebase!");
+});
