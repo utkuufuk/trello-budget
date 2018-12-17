89d2f84d9bfb22f7ed2d9e87d5c23c151a3c2c3e
utkuufuk <utkuufuk@gmail.com>
2018-12-17 23:34:51 +0300

    verify Trello webhook requests

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var crypto = require('crypto');
 const {google} = require('googleapis');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -7,6 +8,8 @@ admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 const settings = {timestampsInSnapshots: true};
 db.settings(settings);
+
+const TRELLO_CALLBACK_URL = "https://us-central1-" + process.env.GCLOUD_PROJECT + ".cloudfunctions.net/transaction"
 
 // parses transaction fields from card name & assigns today if date is not specified
 function parseTransaction(cardName) {
@@ -56,6 +59,20 @@ function updateCells(ssheetId, client, range, transaction) {
     });
 }
 
+// verifies that the webhook request originated from Trello 
+function verifyTrelloWebhookRequest(request, secret) {
+  var base64Digest = function (s) {
+    return crypto.createHmac('sha1', secret).update(s).digest('base64');
+  }
+  if (!("x-trello-webhook" in request.headers)) {
+      return false;
+  }
+  var content = JSON.stringify(request.body) + TRELLO_CALLBACK_URL;
+  var doubleHash = base64Digest(content);
+  var headerHash = request.headers['x-trello-webhook'];
+  return doubleHash === headerHash;
+}
+
 // handles webhooks triggered by the Budget list in Trello
 exports.transaction = functions.https.onRequest((request, response) => {
     // validate request body
@@ -76,10 +93,16 @@ exports.transaction = functions.https.onRequest((request, response) => {
     // retrieve auth token & spreadsheet ID from firestore
     let tokenPromise = db.doc('config/token').get();
     let spreadsheetPromise = db.doc('config/spreadsheet').get();
-    return Promise.all([tokenPromise, spreadsheetPromise])
+    let secretPromise = db.doc('config/auth').get();
+    return Promise.all([tokenPromise, spreadsheetPromise, secretPromise])
     .then(results => {
         const token = results[0].data();
         ssheetId = results[1].data().id;
+        const secret = results[2].data()["trello-secret"];
+        if (!verifyTrelloWebhookRequest(request, secret)) {
+            response.status(403).end();
+            throw new Error("Invalid or unauthorized Trello webhook request."); 
+        }
         client = new google.auth.OAuth2(token.client_id, token.client_secret);
         client.setCredentials(token);
         return readCells(ssheetId, client, "Transactions!B5:E100");
