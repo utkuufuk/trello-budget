bce89ef1a5171271faa8e84fb6603a78a9c2ee02
utkuufuk <utkuufuk@gmail.com>
2018-12-20 00:37:47 +0300

    read username & password from file in post script

--- post.py
+++ post.py
@@ -4,8 +4,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 LOCAL_PORT = 5000
-USERNAME = "utku"
-PASSWORD = "ilikebananas"
 
 def extractId(url):
     start = url.find("spreadsheets/d/")
@@ -16,17 +14,20 @@ def extractId(url):
     return url[(start + 15):end]
 
 if __name__ == "__main__":
-    # read firebase app ID from .firebaserc
+    # read firebase app ID from .firebaserc to build local & remote endpoint addresses
     firebaserc = json.load(open('.firebaserc'))
     appId = firebaserc['projects']['budget']
-
-    # build local & remote endpoint addresses
     localEndpoint = "http://localhost:" + str(LOCAL_PORT) + "/" + appId + "/us-central1/"
     remoteEndpoint = "https://us-central1-" + appId + ".cloudfunctions.net/"
+
+    # read client id & secret from credentials.json to use in HTTP request auth header
+    auth = json.load(open('auth.json'))
+    username = auth["username"]
+    password = auth["password"]
 
     # read arguments & make a post request
     [_, endpoint, function, arg] = sys.argv
     url = (localEndpoint if (endpoint == 'local') else remoteEndpoint) + function
     payload = {'id': extractId(arg)} if function == 'setSheet' else json.load(open(arg))
-    req = requests.post(url, auth=HTTPBasicAuth(USERNAME, PASSWORD), json=payload)
+    req = requests.post(url, auth=HTTPBasicAuth(username, password), json=payload)
     print("Server returned status code:", req.status_code)
